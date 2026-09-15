5effaabc00f5b920f9b1df3194ae20b593267d08
0Fardin10 <fardinmd268@gmail.com>
2026-09-15 14:33:07 +0600

    nextjs

---
+++ nextjs/my-app/app/doc/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const DocPage = () => {
+  return (
+    <div>
+      <h1>Documentation</h1>
+      <p>Welcome to the documentation page.</p>
+    </div>
+  );
+};
+
+export default DocPage;
---
+++ nextjs/my-app/app/showcase/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const ShowcasePage = () => {
+  return (
+    <div>
+      <h1>Documentation For showcase</h1>
+      <p>Welcome to the documentation page for showcase.</p>
+    </div>
+  );
+};
+
+export default ShowcasePage;
