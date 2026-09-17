b89a1ca4aa32e3d7ac05d19d692fcbadf35ee5b7
0Fardin10 <fardinmd268@gmail.com>
2026-09-17 10:39:23 +0600

    SC and CC

---
+++ nextjs/my-new-app/src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start sm:py-16 sm:px-8" >
+        
+        
+        
+        <h2 className="text-5xl font-bold text-zinc-900 dark:text-zinc-100 sm:text-3xl">
+          Welcome to Next.js!
+        </h2>
+      </main>
+    </div>
+  );
+}
