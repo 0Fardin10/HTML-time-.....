e83bf75ea19782ce58fafd6afa71f5e5f3a3b4e1
0Fardin10 <fardinmd268@gmail.com>
2026-09-21 10:39:44 +0600

    Hero and nav bar

--- nextjs-proj/book-vibe/src/app/page.tsx
+++ nextjs-proj/book-vibe/src/app/page.tsx
@@ -1,9 +1,9 @@
 import react from 'react';
+import Banner from './components/homepage/Banner';
 const Page = () => {
   return (
     <div>
-      <h1>Welcome to the Page Component</h1>
-      <p>This is a simple React component.</p>
+    <Banner/>
     </div>
   );
 };
